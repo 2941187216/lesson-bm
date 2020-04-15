315cd37464e42441e5d228911fdfaad557ba6d82
2941187216 <62825347+2941187216@users.noreply.github.com>
2020-04-15 21:49:24 +0800

    Add files via upload

---
+++ add.js
@@ -0,0 +1,6 @@
+// 两数相加 add()
+var addTwoNums = function (a, b) {
+    return a + b;
+};
+const c = addTwoNums(2.0, 5.6);
+console.log(c)
---
+++ add.ts
@@ -0,0 +1,7 @@
+// 两数相加 add()
+
+const addTwoNums = (a :number, b: number) :number => {
+    return a + b;
+}
+
+addTwoNums(2.1,5.6)
